a7646b8235a314cd6e39141281cb8c3901cb8b8c
Ingrid Aquino <ingridaquinosoares@live.com>
2021-12-09 21:16:20 -0300

    Add de escercicios

--- Projeto_Explicação_matrix/src/Matrix.java
+++ Projeto_Explicação_matrix/src/Matrix.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Matrix {
+public class Matrix {
+    public static void main (String[] args){
+
+       // String[] nome = new String["Heloisa" "Duda" "Rafa"] //Array
+
+
+        //Forma incorreta de declarar um Matriz
+       // char[][] nome = new Char[{"HELOISA", "HELO", 21}, {"RAFAEL", "RAFA", 35}, {"EDUARDA", "DUDA", 18}] //MATRIZ
+
+        //Forma correta de fazer a declaração de uma Matriz
+        //TIPO IDENTIFICADOR [][] = New tipo {{TamLinha}, {TamColu}}
+
+        double numeros[][] = new double[3][3];
+
+        int numeros1 [][] = new int[2][3];
+    }
 }
